4a81db149bcc36b90817beabb34a1ad204e708cd
ChrisJonesTC <cfjones91@gmail.com>
2022-09-15 13:04:56 -0400

    initial commit

---
+++ Syntax Sugar/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Syntax_Sugar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Exercise 1
+            //Given the following code, change the code to implement
+            //int answer = 4;
+            //string response;
+            //if (answer < 9)
+            //{
+            //    response = answer + " is less than nine";
+            //}
+            //else
+            //{
+            //    response = answer + " is greater than or equal to nine";
+            //}
+
+            //var answer = 4;
+            //var response = (<) ? $"{}" : $"{}";
+            
+            var answer = 4;
+            var response = (answer < 9) ? $"{answer} is less than nine" : $"{answer} is greater than or equal to nine";
+
+            Console.WriteLine(response);
+
+
+
+
+
+
+        }
+    }
+}
